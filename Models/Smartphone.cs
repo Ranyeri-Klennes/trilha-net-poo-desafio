a0540b2f410cf51c58044064e6c630b44a6fb436
ranyeri-klennes <ranyeri_klennes@hotmail.com>
2022-10-27 14:12:34 -0300

    Atividade concluída com sucesso! Obs: E aprimorada.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,21 +4,45 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            string[] ponto = new string[3];
+            ponto[0] = ".";
+            ponto[1] = "..";
+            ponto[2] = "...";
+
+            foreach(string i in ponto){
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write("Ligando{0}", i);
+                Thread.Sleep(1000);
+            }
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            string[] ponto = new string[3];
+            ponto[0] = ".";
+            ponto[1] = "..";
+            ponto[2] = "...";
+
+            foreach(string i in ponto){
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write("Recebendo ligação{0}", i);
+                Thread.Sleep(1000);
+            }
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
+Console.Clear();
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("-- Nokia --");
+Smartphone nokia = new Nokia(numero: "111-111", modelo: "x1", imei: "01010101", memoria: 120);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("-- Iphone --");
+Smartphone iphone = new Iphone(numero: "222-222", modelo: "x2", imei: "0202020", memoria: 360);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Safari");
